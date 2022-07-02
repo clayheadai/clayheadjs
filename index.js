b72a07c3fa4cdb103d63eef344b1f94c98a3d882
bopowers9 <bopowers9@gmail.com>
2022-07-02 07:42:26 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const { io } = require("socket.io-client");
+
+class AIController {
+    constructor() {
+        this.socket = io(process.env.CLAYHEAD_URL, {
+            query: {
+                key: process.env.CLAYHEAD_API_KEY,
+            },
+        });
+    }
+};
+
+const ai = new AIController();
+console.log(ai);
